123eb11e9e21e6615de8d4c7f4d60ac38cd23d03
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-09 06:23:52 +0700

    finish chapter 1

--- Exercises/Chapter 1/examples.cpp
+++ Exercises/Chapter 1/examples.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
-int totalApples;
 int main() {
-    int numberOfBaskets = 5;
-    int applePerBasket;
-
-    cout << "Enter number apples per baskets: ";
-    cin >> applePerBasket;
-
-    totalApples = numberOfBaskets * applePerBasket;
-    cout << "Number of apples is " << totalApples;
-    return 0;
+    cout << "Hello, World!"; 
+    cout << "My name is Kieran" ;
 }
---
+++ Exercises/Chapter 2/examples.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+int totalApples;
+int main() {
+    int numberOfBaskets = 5;
+    int applePerBasket;
+
+    cout << "Enter number apples per baskets: ";
+    cin >> applePerBasket;
+
+    totalApples = numberOfBaskets * applePerBasket;
+    cout << "Number of apples is " << totalApples;
+    
+    return 0;
+}
