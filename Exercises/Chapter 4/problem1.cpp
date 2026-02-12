88b8553ccb20322c533a8d94b056873cb497fd90
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-12 10:43:00 +0700

    update

--- Exercises/Chapter 4/problem1.cpp
+++ Exercises/Chapter 4/problem1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-float avarage(int a, int b) {
+float average(int a, int b) {
         return (a + b) / 2.0;
     }
 int main() {
     int a, b;
     cin >> a >> b;
-    cout <<avarage(a, b);
+    cout << average(a, b);
     return 0;
     
 }
